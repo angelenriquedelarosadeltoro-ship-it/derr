717a4e63a94f9b6a5ddc97a8f5b5ae63d7a6bb63
angelenriquedelarosadeltoro-ship-it <angelenriquedelarosadeltoro@gmail.com>
2025-09-07 21:52:25 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+led.unplot(0, 0)
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+basic.forever(function () {
+	
+})
